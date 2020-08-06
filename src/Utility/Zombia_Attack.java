e62e6320c2f87dffbed1f2ffa084fa5059dd18f4
kevser87 <kevsireadlig87@outlook.com>
2020-08-05 20:24:07 -0400

    update

--- src/Utility/Zombia_Attack.java
+++ src/Utility/Zombia_Attack.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Zombia_Attack {
-
+  //
         public static void main(String[] args) {
            Scanner input = new Scanner(System.in);
             int[] inhabitants = new int[8];
